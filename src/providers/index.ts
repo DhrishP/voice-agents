147f4353c1b8f9c68e672a6d85804282c38d8c00
DhrishP <parekhdhrish.pg@gmail.com>
2025-03-06 23:44:10 +0530

    remove : providers

--- src/providers/index.ts
+++
@@ -1,6 +0,0 @@
-import { TelephonyProvider } from "./oldtelephony";
-
-export { TelephonyProvider };
-export { default as STTService } from "./stt";
-export { default as AIService } from "./ai";
-export { default as TTSService } from "./tts";
